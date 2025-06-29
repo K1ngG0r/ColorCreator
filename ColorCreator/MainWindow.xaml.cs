460900edbaa07d78191435240f0c6b0d105a5abf
denii <denii@CRM>
2025-06-29 20:43:44 +0700

    Добавьте файлы проекта.

---
+++ ColorCreator/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+
+namespace ColorCreator
+{
+    public partial class MainWindow : Window
+    {
+        private ColorViewModel _ColorViewModel { get; set; }
+
+        public MainWindow()
+        {
+            _ColorViewModel = new ColorViewModel();
+
+            DataContext = _ColorViewModel;
+
+            InitializeComponent();
+        }
+    }
+}
